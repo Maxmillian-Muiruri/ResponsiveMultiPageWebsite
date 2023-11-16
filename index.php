d3ef0f00d7666c7f1a81cd3f37099637784179d3
Maxmillian Muiruri <maxmillianmuiruri@gmail.com>
2023-11-16 17:04:40 +0300

    add php

--- index.php
+++ index.php
@@ -8,15 +8,15 @@ $message = $_POST['message'];
 $email_from = 'maxmillinmuiruri@gmail.com';
 $email_subject = 'New Form submission';
 
-$email_body = "User Name: $name.\n".
-"User Email: $visitor_email.\n".
-"subject: $subject.\n".
-"User Message: $message.\n";
+$email_body = "User Name: $name.\n" .
+    "User Email: $visitor_email.\n" .
+    "subject: $subject.\n" .
+    "User Message: $message.\n";
 
+$to = 'devmaxmillinmuiruri@gmail.com';
+$headers = "from: $email_from \r\n";
+$headers .= "Reply To: $visitor_Email\r\n";
 
-
-
-
+$email($to,$email_subject,$email_body,$headers);
+header("location: contact.html")
 ?>
-
-
